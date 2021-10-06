b54549ed214384c22c10bc2dbadba9f69a42ac2b
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2021-10-06 11:53:12 +0200

    Update __init__.py
    logging import was missing

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the {% if cookiecutter.open_source_license == 'GNU General Public License v3' -%}GNU General Public License{% elif cookiecutter.open_source_license == 'GNU Lesser General Public License v3' -%}GNU Lesser General Public License v3 {% elif cookiecutter.open_source_license == 'GNU Affero General Public License v3' -%}GNU Affero General Public License v3{% endif %}
 # along with {{cookiecutter.project_name}}.  If not, see <https://www.gnu.org/licenses/>.
 """{{cookiecutter.project_short_description}}"""
+import logging
 import logging.config
 import os
 from logging import NullHandler
