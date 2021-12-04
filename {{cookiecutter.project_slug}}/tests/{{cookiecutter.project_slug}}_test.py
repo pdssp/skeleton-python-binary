e90ae0257efd69dd534a829d4dc7316765f2b274
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2021-12-04 18:27:18 +0100

    Improve test

--- {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}_test.py
+++ {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}_test.py
@@ -1,7 +1,38 @@
-import pytest
-
-import {{cookiecutter.project_slug}}
 import logging
+
+#import numpy as np
+import pytest
+import {{cookiecutter.project_slug}}
+#from PIL import Image
+
+
+@pytest.fixture
+def setup():
+    logger.info("----- Init the tests ------")
+
+
+def test_init_setup(setup):
+    logger.info("Setup is initialized")
+
+
+# def assert_images_equal(image_1: str, image_2: str):
+#     img1 = Image.open(image_1)
+#     img2 = Image.open(image_2)
+
+#     # Convert to same mode and size for comparison
+#     img2 = img2.convert(img1.mode)
+#     img2 = img2.resize(img1.size)
+
+#     diff = np.asarray(img1).astype("float") - np.asarray(img2).astype("float")
+
+#     sum_sq_diff = np.sum(diff ** 2)  # type: ignore
+
+#     if sum_sq_diff == 0:
+#         # Images are exactly the same
+#         pass
+#     else:
+#         normalized_sum_sq_diff = sum_sq_diff / np.sqrt(sum_sq_diff)
+#         assert normalized_sum_sq_diff < 0.001
 
 def test_name():
     name = {{cookiecutter.project_slug}}.__name_soft__
@@ -11,4 +42,3 @@ def test_logger():
     loggers = [logging.getLogger()]
     loggers = loggers + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
     assert loggers[0].name == "root"
-    assert loggers[1].name == "{{cookiecutter.project_slug}}"
